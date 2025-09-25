b155c9f4df0de48973637862b0e21816ffa00097
cicero-a <136462951+cicero-a@users.noreply.github.com>
2025-09-25 10:23:03 +0000

    Deploying to gh-pages from @ accelerators-chalmers/accelerators-chalmers.github.io@2d60464405da0a201ba0896f1ebd1fcf27eeaa1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Courses in which our lab is involved",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
