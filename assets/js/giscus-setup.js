18ef167e0145058fc73edbfd0bb7466ca48c028f
cicero-a <136462951+cicero-a@users.noreply.github.com>
2025-10-08 14:01:11 +0000

    Deploying to gh-pages from @ mach-research-lab/mach-research-lab.github.io@62e3e2067d4240932cba62d51fa2ad88b526f6fa 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "machlab-chalmers/machlab-chalmers.github.io",
+    "data-repo": "mach-research-lab/mach-research-lab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
